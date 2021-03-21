f2d26559c255f1ac7f32c50b16958c507aeba5e1
ji sun lee <ellijs@hotmail.com>
2021-03-21 19:29:21 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,14 +1,9 @@
 require 'pry'
 
 class Dog
-  def initialize(name, breed)
-    @name = name
-    @breed = breed
-  end
 
   def name(name)
     @name = name
-    binding.pry
   end
 
   def name=(name)
